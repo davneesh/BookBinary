ca24c98bcf3a2234bdee648c5765b1f4cce3f45c
davneesh <avneeshd09@gmail.com>
2025-01-07 18:28:43 +0530

    upadting index

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pvt = pickle.load(open('pvt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author_x'].values),
+                           image=list(popular_df['Image-URL-M_x'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values))
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
